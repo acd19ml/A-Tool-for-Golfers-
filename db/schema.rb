f587db80b53f4ba0b20bf9725d5108de819c37a9
Jun Zhang <jzhang213@sheffield.ac.uk>
2023-03-13 16:47:15 +0000

    database resetup

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_090742) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_123327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
 
   create_table "annotations", force: :cascade do |t|
     t.decimal "annotationMap"
@@ -82,10 +67,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_090742) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "role", default: "customer", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
