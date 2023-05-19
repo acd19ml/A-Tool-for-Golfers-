62037610a96be71618415f66a8ff5739f9d36b3f
Leon Joyce <ljoyce2@sheffield.ac.uk>
2023-05-19 14:37:51 +0100

    remove line

--- db/migrate/20230310090828_add_foreign_key.rb
+++ db/migrate/20230310090828_add_foreign_key.rb
@@ -6,7 +6,6 @@ class AddForeignKey < ActiveRecord::Migration[7.0]
     add_column :annotations, :user_id, :integer
 
     add_foreign_key :userinfos, :users, column: :user_id
-    add_foreign_key :holes, :courses, column: :course_id
     add_foreign_key :annotations, :holes, column: :hole_id
     add_foreign_key :annotations, :users, column: :user_id
   end
